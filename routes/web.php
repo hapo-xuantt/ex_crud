f0eec6e0235950c5a478377a53ca4862287a55ed
hapo-xuantt <xuantt@haposoft.com>
2020-01-27 22:51:04 +0700

    group prefix

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/products', 'ProductController@index')->name('products.index');
-Route::get('/products/create', 'ProductController@create')->name('products.create');
-Route::post('/products/store', 'ProductController@store')->name('products.store');
-Route::get('/products/{product}', 'ProductController@show')->name('products.show');
-Route::get('/products/{product}/edit', 'ProductController@edit')->name('products.edit');
-Route::put('/products/{product}', 'ProductController@update')->name('products.update');
-Route::delete('/products/{product}', 'ProductController@destroy')->name('products.destroy');
-
+Route::prefix('products')->group(function () {
+    Route::get('/', 'ProductController@index')->name('products.index');
+    Route::get('create', 'ProductController@create')->name('products.create');
+    Route::post('store', 'ProductController@store')->name('products.store');
+    Route::get('{product}', 'ProductController@show')->name('products.show');
+    Route::get('{product}/edit', 'ProductController@edit')->name('products.edit');
+    Route::put('{product}', 'ProductController@update')->name('products.update');
+    Route::delete('{product}', 'ProductController@destroy')->name('products.destroy');
+});
