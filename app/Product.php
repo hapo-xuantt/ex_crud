8b2be9e6d193efa1a4e9e139dfa291ce746f0367
hapo-xuantt <xuantt@haposoft.com>
2020-01-27 23:04:48 +0700

    model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'vendor'];
+    protected $fillable = [
+        'name', 'price', 'vendor'
+    ];
 }
